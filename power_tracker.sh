969df486d33ea2264487e3276233d293bc43ce94
akpp <akkrapovich@gmail.com>
2017-11-16 17:14:58 +0200

    ping router #123

--- power_tracker.sh
+++ power_tracker.sh
@@ -1,9 +1,20 @@
 #!/bin/bash                                                                                                                                                                                                 
-# TODO: ping router
+
+ROUTER_IP='192.168.5.1'
 DATETIME_FORMAT="%Y-%m-%dT%H:%M:%S"
 DATETIME=$(date +$DATETIME_FORMAT)
 IP_ADDR="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-SQL="insert into power_tracker values (INET_ATON('$IP_ADDR'),'$DATETIME')"
 
-mysql -u root power_tracker -e "$SQL"
+
+# if [ ping -c 1 "$ROUTER_IP" >/dev/null ]; then
+
+if ! ping -c 1 "$ROUTER_IP" >/dev/null ; then
+    POWER_ON=0
+else
+    POWER_ON=1
+fi
+
+
+SQL="insert into power_tracker values (INET_ATON('$IP_ADDR'),$POWER_ON,'$DATETIME')"
 echo "$SQL"
+mysql -u root power_tracker -e "$SQL"
