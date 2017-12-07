dddcd0a239872ea5fe9cadba7711afdd4783ae6b
akrapovich <akkrapovich@gmail.com>
2017-12-08 01:25:51 +0200

    wip #123

--- get_stat.sh
+++ get_stat.sh
@@ -2,8 +2,10 @@
 mysql -u root power_tracker -t -e "
 select
 date_format(created_at, '%Y-%m-%d') day,
-SEC_TO_TIME(sum(if(power_on=1,1,0))*60) online,
-SEC_TO_TIME(sum(if(power_on = 0, 1, 0))*60) ofline,
-SEC_TO_TIME(count(*)*60) total
+SEC_TO_TIME(sum(if(power_on=1,1,0))*60) power_on,
+SEC_TO_TIME(sum(if(power_on = 0, 1, 0))*60) power_off,
+SEC_TO_TIME(count(*)*60) total,
+SEC_TO_TIME(sum(if(external_ip,1,0))*60) online,
+count(distinct INET_NTOA(external_ip)) ips
 from power_tracker
 group by year(created_at), month(created_at), day(created_at)"
