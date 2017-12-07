e000fea74d94c7fced3bf2ef16b6f86f51dc69aa
akrapovich <akkrapovich@gmail.com>
2017-12-08 01:05:58 +0200

    human time #123

--- get_stat.sh
+++ get_stat.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-mysql -u root power_tracker -t -e  "select count(*) total, sum(if(power_on=1,1,0)) online, sum(if(power_on = 0, 1, 0)) ofline, date_format(created_at, '%Y-%m-%d') day from power_tracker group by year(created_at), month(created_at), day(created_at)"
+mysql -u root power_tracker -t -e "
+select
+date_format(created_at, '%Y-%m-%d') day,
+SEC_TO_TIME(sum(if(power_on=1,1,0))*60) online,
+SEC_TO_TIME(sum(if(power_on = 0, 1, 0))*60) ofline,
+SEC_TO_TIME(count(*)*60) total
+from power_tracker
+group by year(created_at), month(created_at), day(created_at)"
--- power_tracker.sh
+++ power_tracker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                                                                                                                                                 
 
 ROUTER_IP='192.168.5.1'
-DATETIME_FORMAT="%Y-%m-%dT%H:%M:%S"
+DATETIME_FORMAT="%Y-%m-%dT%H:%M:00"
 DATETIME=$(date +$DATETIME_FORMAT)
 IP_ADDR="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
@@ -11,6 +11,7 @@ else
     POWER_ON=1
 fi
 
+# time in mysql side: SELECT NOW(), CONCAT(LEFT(NOW(), 16), ':00');
 SQL="insert into power_tracker values (INET_ATON('$IP_ADDR'),$POWER_ON,'$DATETIME')"
 echo "$SQL"
 mysql -u root power_tracker -e "$SQL"
