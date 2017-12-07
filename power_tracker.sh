066abeff3cd8fa8fdccab6e1af8c0592b9acb7ee
akrapovich <akkrapovich@gmail.com>
2017-12-08 00:20:56 +0200

    wip #123

--- power_tracker.sh
+++ power_tracker.sh
@@ -5,15 +5,11 @@ DATETIME_FORMAT="%Y-%m-%dT%H:%M:%S"
 DATETIME=$(date +$DATETIME_FORMAT)
 IP_ADDR="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
-
-# if [ ping -c 1 "$ROUTER_IP" >/dev/null ]; then
-
 if ! ping -c 1 "$ROUTER_IP" >/dev/null ; then
     POWER_ON=0
 else
     POWER_ON=1
 fi
-
 
 SQL="insert into power_tracker values (INET_ATON('$IP_ADDR'),$POWER_ON,'$DATETIME')"
 echo "$SQL"
