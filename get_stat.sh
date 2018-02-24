1bc7e8b3831499370361281733982c32a63f2830
akrapovich <akkrapovich@gmail.com>
2018-02-24 20:59:31 +0200

    add sql file #123

--- get_stat.sh
+++ get_stat.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-mysql -u root power_tracker -t -e "
-select
-date_format(created_at, '%Y-%m-%d') day,
-SEC_TO_TIME(sum(if(power_on=1,1,0))*60) power_on,
-SEC_TO_TIME(sum(if(power_on = 0, 1, 0))*60) power_off,
-SEC_TO_TIME(count(*)*60) total,
-SEC_TO_TIME(sum(if(external_ip,1,0))*60) online,
-count(distinct INET_NTOA(external_ip)) ips
-from power_tracker
-group by year(created_at), month(created_at), day(created_at)"
+mysql -u root power_tracker -t -e "$(< script.sql)"
